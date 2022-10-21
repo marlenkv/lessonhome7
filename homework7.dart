869cb6a02285eb09fc32a6819e3694f8fa7e0572
marlenkv <kumarbekovmarlen@gmail.com>
2022-10-21 15:34:48 +0600

    first commit

---
+++ homework7.dart
@@ -0,0 +1,32 @@
+void main() {
+  int i = 1;
+  do {
+    print(i);
+
+    i++;
+  } while (i < 101);
+  for (int i = 1; i < 101; i++) {
+    print(i);
+  }
+  i = 11;
+  do {
+    print(i);
+
+    i++;
+  } while (i < 34);
+  for (int i = 11; i < 34; i++) {
+    print(i);
+  }
+  int i = 0;
+  do {
+    if (i.isEven) ;
+
+    i++;
+  } while (i < 100);
+  for (int i = 0; i < 100; i++) {
+    if (i.isEven) {
+      
+    }
+  }
+  print(i);
+}
